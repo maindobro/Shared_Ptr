8030c1ddc56763cb4743c3b651229ab1ef9423e3
tpabahatp <nekros011@gmail.com>
2016-12-16 12:06:22 +0300

    Update shared_ptr.cpp

--- include/shared_ptr.cpp
+++ include/shared_ptr.cpp
@@ -38,16 +38,16 @@ template <typename T>
 shared_ptr<T>::shared_ptr(T* ptr) : ptr_(ptr), counter_(new size_t(1)) {}
 
 template <typename T>
-shared_ptr<T>::shared_ptr(shared_ptr const& x) : ptr_(x.ptr_), counter_(x.counter_) 
+shared_ptr<T>::shared_ptr(shared_ptr const& other) : ptr_(other.ptr_), counter_(other.counter_) 
 {
 	if(counter_ != nullptr) ++(*counter_);
 }
 
 template <typename T>
-shared_ptr<T>::shared_ptr(shared_ptr&& x) : ptr_(x.ptr_), counter_(x.counter_)
+shared_ptr<T>::shared_ptr(shared_ptr&& other) : ptr_(other.ptr_), counter_(other.counter_)
 {
-	x.ptr_ = nullptr;
-	x.counter_ = nullptr;
+	other.ptr_ = nullptr;
+	other.counter_ = nullptr;
 }
 
 template <typename T>
@@ -61,19 +61,65 @@ shared_ptr<T>::~shared_ptr()
 }
 
 template <typename T>
-auto shared_ptr<T>::operator = (shared_ptr const& x) -> shared_ptr&
+auto shared_ptr<T>::operator=(shared_ptr const& other) -> shared_ptr&
 {
 	if (this != &x)
 	{
-		(shared_ptr<T>(x)).swap(*this);
+		(shared_ptr<T>(other)).swap(*this);
 	}
 	return *this;
 }
 
 template <typename T>
-auto shared_ptr<T>::operator = (shared_ptr&& x) -> shared_ptr&
+auto shared_ptr<T>::operator = (shared_ptr&& other) -> shared_ptr&
 {
-	if (this != &x) 
-		swap(x);
+	if (this != &other) 
+		(shared_ptr(std::move(other))).swap(*this);
 	return *this;
+}
+
+template <typename T>
+auto shared_ptr<T>::swap(shared_ptr& other) -> void
+{
+	std::swap(ptr_, other.ptr_);
+	std::swap(counter_, other.counter_);
+}
+
+template <typename T>
+auto shared_ptr<T>::reset() -> void
+{
+	if (ptr_ != nullptr && counter_ != nullptr && --(*counter_) == 0)
+	{
+		delete ptr_;
+		delete counter_;
+	}
+	ptr_ = nullptr;
+	counter_ = nullptr;
+}
+
+template <typename T>
+auto shared_ptr<T>::get() const -> T*
+{
+	return ptr_;
+}
+
+template <typename T>
+auto shared_ptr<T>::operator ->() const -> T*
+{
+	if (ptr_ != nullptr) return ptr_;
+	else throw ("nullptr...");
+}
+
+template <typename T>
+auto shared_ptr<T>::operator *() const -> T&
+{
+	if (ptr_ != nullptr) return *ptr_;
+	else throw ("nullptr..");
+}
+
+template <typename T>
+auto shared_ptr<T>::countref() const -> size_t
+{
+	if (counter_ != nullptr) return *counter_;
+	else return 0;
 }
